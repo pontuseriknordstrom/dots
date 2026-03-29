7c9827a374838585da9a869c229a56537a6319de
pontus <pontus@fastmail.org>
2026-03-29 18:08:38 +0200

    chore(repo): start from scratch

--- modules/fonts.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-
-{
-  fonts.packages = with pkgs; [
-    iosevka-bin
-    noto-fonts
-    noto-fonts-color-emoji
-  ];
-}
--- modules/networking.nix
+++
@@ -1,6 +0,0 @@
-{ ... }:
-
-{
-  networking.hostName = "descartes";
-  networking.networkmanager.enable = true;
-}
